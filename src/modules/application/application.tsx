8a44551dc0169867a8e3dd9d6c745c511b172880
Vegard Falldalen <vegfall@gmail.com>
2024-02-14 13:49:12 +0100

    Added stadia map

--- src/modules/application/application.tsx
+++ src/modules/application/application.tsx
@@ -1,4 +1,10 @@
-import React, { MutableRefObject, useEffect, useRef, useState } from "react";
+import React, {
+  MutableRefObject,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
 import TileLayer from "ol/layer/Tile";
 import { OSM } from "ol/source";
 import "ol/ol.css";
@@ -15,17 +21,25 @@ import { BaseLayerDropdown } from "../baseLayer/baseLayerDropdown";
 
 export function Application() {
   const mapRef = useRef() as MutableRefObject<HTMLDivElement>;
-  const [layers, setLayers] = useState<Layer[]>([
-    new TileLayer({ source: new OSM() }),
-  ]);
+
+  const [baseLayer, setBaseLayer] = useState<Layer>(
+    () => new TileLayer({ source: new OSM() }),
+  );
+
+  const [vectorLayers, setVectorLayers] = useState<Layer[]>([]);
+
+  const allLayers = useMemo(
+    () => [baseLayer, ...vectorLayers],
+    [baseLayer, vectorLayers],
+  );
 
   useEffect(() => {
     map.setTarget(mapRef.current);
   }, []);
 
   useEffect(() => {
-    map.setLayers(layers);
-  }, [layers]);
+    map.setLayers(allLayers);
+  }, [allLayers]);
 
   function handleFocusUser(e: React.MouseEvent) {
     e.preventDefault();
@@ -41,7 +55,14 @@ export function Application() {
   }
 
   return (
-    <MapContext.Provider value={{ map, layers, setLayers }}>
+    <MapContext.Provider
+      value={{
+        map,
+        layers: vectorLayers,
+        setLayers: setVectorLayers,
+        setBaseLayer,
+      }}
+    >
       <header>
         <h1>My Awesome Map!</h1>
       </header>
--- src/modules/baseLayer/baseLayerDropdown.tsx
+++ src/modules/baseLayer/baseLayerDropdown.tsx
@@ -1,18 +1,32 @@
-import React, { useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import TileLayer from "ol/layer/Tile";
+import { OSM, StadiaMaps } from "ol/source";
+import { MapContext } from "../map/mapContext";
 
 export function BaseLayerDropdown() {
+  const { setBaseLayer } = useContext(MapContext);
+
   const baseLayersOptions = [
     {
       id: "osm",
       name: "Open Street Map",
+      layer: new TileLayer({ source: new OSM() }),
     },
     {
       id: "stadia",
       name: "Stadia",
+      layer: new TileLayer({
+        source: new StadiaMaps({
+          layer: "outdoors",
+          retina: true,
+        }),
+      }),
     },
   ];
 
   const [selectedLayer, setSelectedLayer] = useState(baseLayersOptions[0]);
+
+  useEffect(() => setBaseLayer(selectedLayer.layer), [selectedLayer]);
 
   return (
     <div>
@@ -28,7 +42,6 @@ export function BaseLayerDropdown() {
           <option value={id}>{name}</option>
         ))}
       </select>
-      {selectedLayer.name}
     </div>
   );
 }
