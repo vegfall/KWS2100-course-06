255023d4d2f7ea31116d9090cb40d06543536c61
Vegard Falldalen <vegfall@gmail.com>
2024-02-14 13:27:25 +0100

    Working selector

--- src/modules/baseLayer/baseLayerDropdown.tsx
+++ src/modules/baseLayer/baseLayerDropdown.tsx
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 export function BaseLayerDropdown() {
+  const baseLayersOptions = [
+    {
+      id: "osm",
+      name: "Open Street Map",
+    },
+    {
+      id: "stadia",
+      name: "Stadia",
+    },
+  ];
+
+  const [selectedLayer, setSelectedLayer] = useState(baseLayersOptions[0]);
+
   return (
-    <select>
-      <option>First map</option>
-    </select>
+    <div>
+      <select
+        onChange={(e) =>
+          setSelectedLayer(
+            baseLayersOptions.find((l) => l.id === e.target.value)!,
+          )
+        }
+        value={selectedLayer.id}
+      >
+        {baseLayersOptions.map(({ id, name }) => (
+          <option value={id}>{name}</option>
+        ))}
+      </select>
+      {selectedLayer.name}
+    </div>
   );
 }
